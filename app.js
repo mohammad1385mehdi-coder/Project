5c632a3267fb16f05d3f90fbe745f8233b7ea8a6
mohammad1385mehdi-coder <mohammad1385mehdi@gmail.com>
2025-12-24 11:59:53 +0330

    project-1    javascreept

---
+++ app.js
@@ -0,0 +1,12 @@
+var user = {
+    firstName: 'MohammadMehdi',
+    lastName: 'Golparvar',
+    age:20,
+    hasCar: false,
+    gender: 'male',
+    jobs: ['student', 'developer'],
+    address: {
+        state: 'Isfahan',
+        city: 'esfahan',
+    }
+}
